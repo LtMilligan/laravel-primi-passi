112f7ca4afba4ee3d1bbc8aed9ff60497c903563
LtMilligan <gioele.miscia@gmail.com>
2024-09-24 01:41:21 +0200

    [UPD] Milestone bonus done

--- resources/views/page1.blade.php
+++ resources/views/page1.blade.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Laravel Primi Passi</title>
+    <title>Document</title>
 </head>
 <body>
     <div class="container mt-5">
         <div class="row">
             <div class="col-12">
-                <div class="content pt-3">
+                <div class="content">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav d-flex justify-content-evenly" style="display: flex; width: 100%;">
                                 <li class="nav-item active">
-                                    <h2><a class="nav-link" href="{{ route('page1') }}">Page
-                                            1</a></h2>
+                                    <h2><a class="nav-link" href="{{ route('home') }}">Home</a></h2>
                                 </li>
                                 <li class="nav-item">
                                     <h2><a class="nav-link" href="{{ route('page2') }}">Page 2</a></h2>
@@ -27,6 +26,9 @@
                             </ul>
                         </div>
                     </nav>
+                </div>
+                <div class="content d-flex justify-content-center">
+                    <img src='{{ $img1 }}' alt="">
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,18 @@ Route::get('/', function () {
     $message = 'Hello Laravel';
     return view('home', compact('message'));
 })->name('home');
+
+Route::get('/img1', function () {
+    $img1 = 'https://placehold.co/600x400?text=Page1';
+    return view('page1', compact('img1'));
+})->name('page1');
+
+Route::get('/img2', function () {
+    $img2 = 'https://placehold.co/600x400?text=Page2';
+    return view('page2', compact('img2'));
+})->name('page2');
+
+Route::get('/img3', function () {
+    $img3 = 'https://placehold.co/600x400?text=Page3';
+    return view('page3', compact('img3'));
+})->name('page3');
